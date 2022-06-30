d760ddcc02449606f7bbf6b0d94c228241b0c1f1
samraber <sam9985246327@gmail.com>
2022-06-30 09:26:32 +0800

    setting up keystone and typescript

---
+++ backend/keystone.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+  },
+  lists: createSchema({
+    // Schema items go in here
+  }),
+  ui: {
+    isAccessAllowed: () => true,
+  },
+});
